9a052bb84356020548c26a7db0269afd04e60b1d
Simeon Jenkins <55206+smuj@users.noreply.github.com>
2019-05-29 19:00:06 +0100

    Restructure top-level code into scene object

--- fourhills.py
+++ fourhills.py
@@ -2,88 +2,86 @@ import sys
 import re
 import yaml
 import click
-from typing import Optional, List, Tuple
+from typing import List, Tuple
 from setting import Setting
 from stats import StatBlock
 from npc import Npc
-from fourhills_exceptions import FourhillsFileLoadError, FourhillsSettingStructureError
+from fourhills_exceptions import FourhillsFileLoadError
 
-BATTLE_FILENAME = "battle.yaml"
-
-
-def load_battle_info(filename: str) -> Tuple[List[Tuple[str, int]], List[str]]:
-    """Load info about a battle from a file and return the details.
-
-    Parameters
-    ----------
-    filename : str
-        Filename of the YAML file to load battle info from.
-
-    Returns
-    -------
-    list of (str, int)
-        List of monster names and the number of each
-    """
-    with open(filename) as f:
-        try:
-            battle_info = yaml.safe_load(f)
-        except yaml.YAMLError as exc:
-            raise FourhillsFileLoadError(f"Error loading from {filename}.") from exc
-
-        # Stores the list of monster names and numbers
-        monster_info = []
-
-        # If there was a monsters section, load the monsters
-        if "monsters" in battle_info:
-            for monster_name_number in battle_info["monsters"]:
-                # See if it matches the expected format, extracting name and number
-                match = re.match(r"^(\w*)(?: ?x?(\d+))?$", monster_name_number)
-                if not match:
-                    raise FourhillsFileLoadError("Error parsing monster in battle file")
-                # Get the name of the monster and how many there are. If there
-                # wasn't a number, assume 1 monster.
-                name = match[1]
-                number = match[2] or "1"
-                # Convert to int
-                try:
-                    number = int(number)
-                except ValueError as exc:
-                    raise FourhillsFileLoadError(
-                        f"Error parsing number for monster {name}"
-                    ) from exc
-                # Add to the list
-                monster_info.append((name, number))
-
-        # Stores the list of NPC names
-        npc_info = battle_info["npcs"] or []
-
-        return monster_info, npc_info
+SCENE_FILENAME = "battle.yaml"
 
 
-def battle():
+class Scene:
+    """Represents a particular location in the world."""
 
-    setting = Setting()
+    def __init__(
+        self, monster_names_quantities: List[Tuple[str, int]], npc_names: List[str]
+    ):
+        """Initialise the object."""
+        self.monster_names_quantities = monster_names_quantities
+        self.npc_names = npc_names
+        self.setting = Setting()
 
-    try:
-        monster_info, npc_info = load_battle_info(BATTLE_FILENAME)
-    except FileNotFoundError:
-        raise FourhillsSettingStructureError(
-            f"No '{BATTLE_FILENAME}' battle file found."
-        )
+    @classmethod
+    def from_file(cls, filename: str):
+        """Load scene info from a file and return a Scene instance.
 
-    stat_strings = list()
+        Parameters
+        ----------
+        filename : str
+            Filename of the YAML file to load scene info from.
+        """
+        with open(filename) as f:
+            try:
+                scene_info = yaml.safe_load(f)
+            except yaml.YAMLError as exc:
+                raise FourhillsFileLoadError(f"Error loading from {filename}.") from exc
 
-    for monster_name, quantity in monster_info:
-        monster = StatBlock.from_name(monster_name, setting)
-        stat_strings.extend(monster.summary_info(setting.pane_width, quantity))
-        stat_strings.extend(monster.battle_info(setting.pane_width))
+            # Stores the list of monster names and numbers
+            monster_info = []
 
-    for npc_name in npc_info:
-        npc = Npc.from_name(npc_name, setting)
-        stat_strings.extend(npc.summary_info(setting.pane_width))
-        stat_strings.extend(npc.battle_info(setting.pane_width))
+            # If there was a monsters section, load the monsters
+            if "monsters" in scene_info:
+                for monster_name_number in scene_info["monsters"]:
+                    # See if it matches the expected format, extracting name and number
+                    match = re.match(r"^(\w*)(?: ?x?(\d+))?$", monster_name_number)
+                    if not match:
+                        raise FourhillsFileLoadError(
+                            "Error parsing monster in scene file"
+                        )
+                    # Get the name of the monster and how many there are. If there
+                    # wasn't a number, assume 1 monster.
+                    name = match[1]
+                    number = match[2] or "1"
+                    # Convert to int
+                    try:
+                        number = int(number)
+                    except ValueError as exc:
+                        raise FourhillsFileLoadError(
+                            f"Error parsing number for monster {name}"
+                        ) from exc
+                    # Add to the list
+                    monster_info.append((name, number))
 
-    click.echo_via_pager("\n".join(stat_strings))
+            # Stores the list of NPC names
+            npc_info = scene_info["npcs"] or []
+
+            return cls(monster_info, npc_info)
+
+    def display_battle(self):
+        stat_strings = list()
+
+        for monster_name, quantity in self.monster_names_quantities:
+            monster = StatBlock.from_name(monster_name, self.setting)
+            stat_strings.extend(monster.summary_info(self.setting.pane_width, quantity))
+            stat_strings.extend(monster.battle_info(self.setting.pane_width))
+
+        for npc_name in self.npc_names:
+            npc = Npc.from_name(npc_name, self.setting)
+            stat_strings.extend(npc.summary_info(self.setting.pane_width))
+            stat_strings.extend(npc.battle_info(self.setting.pane_width))
+
+        click.echo_via_pager("\n".join(stat_strings))
 
 
 def print_usage():
@@ -95,9 +93,10 @@ def print_location():
 
 
 def main():
+    scene = Scene.from_file(SCENE_FILENAME)
     if len(sys.argv) > 1:
         if sys.argv[1] in ["b", "battle"]:
-            battle()
+            scene.display_battle()
         else:
             print_usage()
     else:
