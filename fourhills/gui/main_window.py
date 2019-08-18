034f8d684b27476919ff4c2387b572b95af93e85
Michael Hart <michael.hart1994@gmail.com>
2019-08-18 08:50:26 +0100

    Change to using code only instead of Designer

---
+++ fourhills/gui/main_window.py
@@ -0,0 +1,49 @@
+from PySide2 import QtWidgets
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget
+
+from fourhills.gui.notes_pane import NotesPane
+
+
+class TestWidget(QWidget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.test_lbl = QtWidgets.QLabel("Hello from TestWidget!", parent=self)
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        # Basic window construction
+        self.setObjectName("MainWindow")
+        self.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+
+        # Root layout creation
+        self.root_layout_widget = QtWidgets.QWidget(self.centralwidget)
+        self.root_layout_widget.setObjectName("root_layout_widget")
+        self.root_layout = QtWidgets.QVBoxLayout(self.root_layout_widget)
+        self.root_layout.setObjectName("root_layout")
+
+        # Content placeholder
+        self.content_lbl = QtWidgets.QLabel("Content Placeholder", parent=self)
+        self.root_layout.addWidget(self.content_lbl)
+
+        # Notes pane
+        self.notes_pane = NotesPane(parent=self)
+        self.root_layout.addWidget(self.notes_pane)
+
+        # Final window setup
+        self.setCentralWidget(self.centralwidget)
+        self.centralwidget.setLayout(self.root_layout)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication([])
+    app.setApplicationName("FourHills GUI")
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ fourhills/gui/notes_pane.py
@@ -0,0 +1,27 @@
+"""Define a Qt Widget for the Notes Pane"""
+
+from PySide2 import QtWidgets
+from PySide2.QtWidgets import QWidget
+
+
+class NotesPane(QWidget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout = QtWidgets.QVBoxLayout()
+
+        # Add file information
+        self.file_layout = QtWidgets.QHBoxLayout()
+        self.load_btn = QtWidgets.QPushButton("Load File")
+        self.save_btn = QtWidgets.QPushButton("Save File")
+        self.file_path = QtWidgets.QLineEdit("<none>")
+        self.file_path.setReadOnly(True)
+        self.file_layout.addWidget(self.load_btn)
+        self.file_layout.addWidget(self.save_btn)
+        self.file_layout.addWidget(self.file_path)
+        self.layout.addLayout(self.file_layout)
+
+        # Add notes text box
+        self.notes_text = QtWidgets.QTextEdit()
+        self.layout.addWidget(self.notes_text)
+
+        self.setLayout(self.layout)
